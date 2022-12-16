21bab93f04ef253a4f6a9ef55e3fb81627dd2860
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-16 23:21:00 +0000

    queryChatgpt function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world")
+import("dotenv").then(dotenv => dotenv.config())
+
+const queryChatgpt = async (query: string) => {
+	const { ChatGPTAPIBrowser } = await import("chatgpt")
+	const chatgptApi = new ChatGPTAPIBrowser({
+		email: process.env.OPENAI_EMAIL as string,
+		password: process.env.OPENAI_PASSWORD as string,
+	})
+	await chatgptApi.init()
+	return await chatgptApi.sendMessage(query)
+}
+
+export {}
