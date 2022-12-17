49779c7ffa761942c417fbcf45c7aefe89a06c15
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-17 14:13:03 +0000

    post api working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import("dotenv").then(dotenv => dotenv.config())
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
 
 const queryChatgpt = async (query: string) => {
 	const { ChatGPTAPIBrowser } = await import("chatgpt")
@@ -9,5 +15,16 @@ const queryChatgpt = async (query: string) => {
 	await chatgptApi.init()
 	return await chatgptApi.sendMessage(query)
 }
+
+app.post("/askchatgpt", async (req, res) => {
+	const query = req.body.query
+	const response = await queryChatgpt(query)
+	res.json(response)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+})
 
 export {}
